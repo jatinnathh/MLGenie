6047f24ba43d46bf3c2bf1d623d7a1c62a677b1e
jatinnathh <jatinnath1111@gmail.com>
2025-07-08 00:44:54 +0530

    july 8

---
+++ pages/DL_training.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("DL Training")
+    st.write("Here you will train deep learning models.")
---
+++ pages/ML_training.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("ML Training")
+    st.write("Here you will train ML models.")
